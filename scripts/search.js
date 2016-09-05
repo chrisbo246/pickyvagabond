3be96f75242e5109ae5d7a6c3fd3c82a898f85dc
Chrisbo <chrisbo246@gmail.com>
2016-09-05 21:14:36 +0200

    Commit

--- scripts/search.js
+++ scripts/search.js
@@ -7,8 +7,13 @@
             var appendString = '';
             for (var i = 0; i < results.length; i++) {  // Iterate over the results
                 var item = store[results[i].ref];
-                appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-                appendString += '<p>' + item.content.substring(0, 150) + ((item.content.length > 150) ? '&#8230;' : '') + '</p></li>';
+                appendString += '<li>'
+                    + '<h3>'
+                    + '<a href="' + item.url + '">' + item.title + '</a>'
+                    + ' <span class="label label-' + item.collection + '">' + site.collections[item.collection].title + '</span>'
+                    + '</h3>'
+                    + '<p>' + item.content.substring(0, 150) + ((item.content.length > 150) ? '&#8230;' : '') + '</p>'
+                    + '</li>';
             }
 
             searchResults.innerHTML = appendString;
